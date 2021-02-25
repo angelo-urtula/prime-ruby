f24173cc4a752fbf7e144e620c834d22c56c64c8
angelo <aurtula1@gmail.com>
2021-02-25 16:01:25 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+    if num > 1
+    (2..(num - 1)).each do |n|
+        return false if num % n == 0
+    end
+    elsif num <= 1
+        return false
+    end
+    true
+end
